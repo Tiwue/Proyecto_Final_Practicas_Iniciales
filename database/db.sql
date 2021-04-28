1324438c5cc741e15befb6e283fdab848fbe9f24
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2021-04-27 20:03:31 -0600

    Create db.sql

---
+++ database/db.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-04-2021 a las 03:59:45
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `biblioteca`
+--
+
+CREATE TABLE `biblioteca` (
+  `idBiblioteca` int(11) NOT NULL,
+  `Usuario_idUsuario` int(11) NOT NULL,
+  `Juego_idJuego` int(11) NOT NULL,
+  `Puntuacion` int(11) NOT NULL,
+  `Opinion` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `idComentarios` int(11) NOT NULL,
+  `Usuario_idUsuario` int(11) NOT NULL,
+  `Publicacion_idPublicacion` int(11) NOT NULL,
+  `Comentario` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consola`
+--
+
+CREATE TABLE `consola` (
+  `idConsola` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juego`
+--
+
+CREATE TABLE `juego` (
+  `idJuego` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Descripcion` varchar(45) NOT NULL,
+  `Cartucho` varchar(45) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Consola_idConsola` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publicacion`
+--
+
+CREATE TABLE `publicacion` (
+  `idPublicacion` int(11) NOT NULL,
+  `Fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Usuario_idUsuario` int(11) NOT NULL,
+  `Juego_idJuego` int(11) NOT NULL,
+  `Comentario` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Username` varchar(45) NOT NULL,
+  `Correo` varchar(45) NOT NULL,
+  `Contrasenia` varchar(45) NOT NULL,
+  `Biografia` varchar(45) NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `biblioteca`
+--
+ALTER TABLE `biblioteca`
+  ADD PRIMARY KEY (`idBiblioteca`),
+  ADD KEY `Usuario_idUsuario` (`Usuario_idUsuario`,`Juego_idJuego`),
+  ADD KEY `Juego_idJuego` (`Juego_idJuego`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`idComentarios`),
+  ADD KEY `Usuario_idUsuario` (`Usuario_idUsuario`,`Publicacion_idPublicacion`),
+  ADD KEY `Publicacion_idPublicacion` (`Publicacion_idPublicacion`);
+
+--
+-- Indices de la tabla `consola`
+--
+ALTER TABLE `consola`
+  ADD PRIMARY KEY (`idConsola`);
+
+--
+-- Indices de la tabla `juego`
+--
+ALTER TABLE `juego`
+  ADD PRIMARY KEY (`idJuego`),
+  ADD KEY `Consola_idConsola` (`Consola_idConsola`);
+
+--
+-- Indices de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD PRIMARY KEY (`idPublicacion`),
+  ADD KEY `Usuario_idUsuario` (`Usuario_idUsuario`,`Juego_idJuego`),
+  ADD KEY `Juego_idJuego` (`Juego_idJuego`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `biblioteca`
+--
+ALTER TABLE `biblioteca`
+  MODIFY `idBiblioteca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `idComentarios` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `consola`
+--
+ALTER TABLE `consola`
+  MODIFY `idConsola` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `juego`
+--
+ALTER TABLE `juego`
+  MODIFY `idJuego` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  MODIFY `idPublicacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `biblioteca`
+--
+ALTER TABLE `biblioteca`
+  ADD CONSTRAINT `biblioteca_ibfk_1` FOREIGN KEY (`Usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `biblioteca_ibfk_2` FOREIGN KEY (`Juego_idJuego`) REFERENCES `juego` (`idJuego`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`Publicacion_idPublicacion`) REFERENCES `publicacion` (`idPublicacion`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`Usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `juego`
+--
+ALTER TABLE `juego`
+  ADD CONSTRAINT `juego_ibfk_1` FOREIGN KEY (`Consola_idConsola`) REFERENCES `consola` (`idConsola`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD CONSTRAINT `publicacion_ibfk_1` FOREIGN KEY (`Juego_idJuego`) REFERENCES `juego` (`idJuego`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `publicacion_ibfk_2` FOREIGN KEY (`Usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
