45ee30c1dc4d267be3dfc80a6c9e9dc8bccd940c
Steven <stevengonzalez088@gmail.com>
2021-04-28 21:33:31 -0600

    port config
    configuraciones para la inicializacion de la app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.log("WORKS")
+import express, {Application} from 'express';
+
+class Server{
+    public app: Application ; 
+    constructor(){
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    config(): void{
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    routes(): void{
+    }
+
+    start():void{
+        this.app.listen(this.app.get('port'),()=>{
+        console.log('Prueba de puerto', this.app.get('port'))});
+    }
+    }
+
+const server = new Server();
+server.start();
