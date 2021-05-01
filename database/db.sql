4739c3a6ea4c4d97b5cfc31e439a9636dcfd678b
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2021-05-01 15:10:10 -0600

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-04-2021 a las 03:59:45
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `prueba`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `biblioteca`
---
+CREATE DATABASE db;
+USE db;
 
 CREATE TABLE `biblioteca` (
   `idBiblioteca` int(11) NOT NULL,
